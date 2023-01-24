0c240c0c6444274d3d7c71deb5b304a58b1d30bd
Jan Przebor <przebot@gmail.com>
2023-01-24 15:39:08 +0100

    Sometimes I don't know what I'm doing tbh

--- src/unet/datasets/loveda.py
+++ src/unet/datasets/loveda.py
@@ -18,9 +18,8 @@ def normalize(input_image, input_mask):
 
 
 def load_image_train(x):
-    (train_dir, number) = x
-    input_image = tf.image.resize(tf.io.decode_png(os.path.join(train_dir, "images", str(number+".png"))), IMAGE_SIZE)
-    input_mask = tf.image.resize(tf.io.decode_png(os.path.join(train_dir, "masks", str(number+".png"))), IMAGE_SIZE)
+    input_image = tf.image.resize(tf.io.decode_png(os.path.join(TRAIN_DIR, "images", str(x+".png"))), IMAGE_SIZE)
+    input_mask = tf.image.resize(tf.io.decode_png(os.path.join(TRAIN_DIR, "masks", str(x+".png"))), IMAGE_SIZE)
 
     if tf.random.uniform(()) > 0.5:
         input_image = tf.image.flip_left_right(input_image)
@@ -32,9 +31,8 @@ def load_image_train(x):
 
 
 def load_image_validate(x):
-    (validate_dir, number) = x
-    input_image = tf.image.resize(tf.io.decode_png(os.path.join(validate_dir, "images", str(number+".png"))), IMAGE_SIZE)
-    input_mask = tf.image.resize(tf.io.decode_png(os.path.join(validate_dir, "masks", str(number+".png"))), IMAGE_SIZE)
+    input_image = tf.image.resize(tf.io.decode_png(os.path.join(VALIDATE_DIR, "images", str(x+".png"))), IMAGE_SIZE)
+    input_mask = tf.image.resize(tf.io.decode_png(os.path.join(VALIDATE_DIR, "masks", str(x+".png"))), IMAGE_SIZE)
 
     input_image, input_mask = normalize(input_image, input_mask)
 
@@ -42,12 +40,16 @@ def load_image_validate(x):
 
 
 def load_data(train_dir, validate_dir, train_count, validate_count, **kwargs) -> Tuple[tf.data.Dataset, tf.data.Dataset]:
+    global TRAIN_DIR
+    global VALIDATE_DIR
+    TRAIN_DIR = train_dir
+    VALIDATE_DIR = validate_dir
     train_l = []
     for i in range(train_count):
-        train_l.append((train_dir, i))
+        train_l.append(i)
     validate_l = []
     for i in range(validate_count):
-        validate_l.append((validate_dir, i))
+        validate_l.append(i)
     
     train = tf.data.Dataset.from_tensor_slices(train_l)
     validate = tf.data.Dataset.from_tensor_slices(validate_l)
